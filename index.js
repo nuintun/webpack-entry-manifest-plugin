3bd52c558c6c0658426c14d25dc47a2173c4a34d
nuintun <nuintun@qq.com>
2018-05-18 18:22:14 +0800

    :art: Update files

--- index.js
+++ index.js
@@ -7,70 +7,96 @@
  * @see https://github.com/jakedahm/webpack-asset-manifest-plugin
  */
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs-extra');
+const path = require('path');
 
+/**
+ * @function unixify
+ * @description Convert path separators to posix/unix-style forward slashes.
+ * @param {string} path
+ * @returns {string}
+ */
+function unixify(path) {
+  return path.replace(/\\/g, '/');
+}
+
+/**
+ * @class WebpackEntryManifestPlugin
+ */
 class WebpackEntryManifestPlugin {
-  constructor(opts) {
-    this.name = "WebpackEntryManifestPlugin";
+  /**
+   * @constructor
+   * @param {Object} options
+   */
+  constructor(options) {
+    this.name = 'WebpackEntryManifestPlugin';
     this.options = Object.assign(
       {
-        filename: "manifest.json",
-        outputPath: null
+        basePath: null,
+        outputPath: null,
+        publicPath: null,
+        filename: 'manifest.json',
+        serialize: manifest => JSON.stringify(manifest, null, 2)
       },
-      opts || {}
+      options
     );
   }
 
-  apply(compiler) {
-    compiler.hooks.emit.tap(this.name, compilation => {
-      const manifest = compilation.chunkGroups.reduce((manifest, group) => {
-        if (group.name) {
-          const groupFiles = group
-            .getFiles()
-            .map(file => path.join(compiler.options.output.publicPath, file));
+  /**
+   * @private
+   * @method generateManifest
+   * @param {Compiler} compiler
+   * @param {Compilation} compilation
+   */
+  generateManifest(compiler, compilation) {
+    const options = this.options;
+    const basePath = options.basePath || '';
+    const publicPath = options.publicPath || compiler.options.output.publicPath;
 
-          manifest[group.name] = {
-            files: groupFiles,
-            chunks: group.getChildren().reduce((chunks, chunk) => {
-              const files = chunk.chunks.reduce((items, item) => {
-                item.files.forEach(file => {
-                  const item = path.join(
-                    compiler.options.output.publicPath,
-                    file
-                  );
-                  if (
-                    chunks.indexOf(item) === -1 &&
-                    groupFiles.indexOf(item) === -1
-                  ) {
-                    items.push(
-                      path.join(compiler.options.output.publicPath, file)
-                    );
-                  }
-                });
+    // Get manifest
+    const manifest = compilation.chunkGroups.reduce((manifest, group) => {
+      if (group.name) {
+        const chunks = group.chunks;
+        const name = basePath + group.name;
+        const groupFiles = group.getFiles().map(file => publicPath + file);
 
-                return items;
-              }, []);
-              
-              return chunks.concat(files);
-            }, [])
-          };
-        }
+        manifest[name] = {
+          files: groupFiles,
+          chunks: group.getChildren().reduce((chunks, chunk) => {
+            const files = chunk.chunks.reduce((items, item) => {
+              item.files.forEach(file => {
+                const item = publicPath + file;
 
-        return manifest;
-      }, {});
+                if (chunks.indexOf(item) === -1 && groupFiles.indexOf(item) === -1) {
+                  items.push(publicPath + file);
+                }
+              });
 
-      if (Object.keys(manifest).length > 0) {
-        const outputPath = this.options.outputPath || compiler.outputPath;
-        
-        compiler.outputFileSystem.mkdirp(outputPath, () => {
-          fs.writeFileSync(
-            path.join(outputPath, "manifest.json"),
-            JSON.stringify(manifest)
-          );
-        });
+              return items;
+            }, []);
+
+            return chunks.concat(files);
+          }, [])
+        };
       }
-    });
+
+      return manifest;
+    }, Object.create(null));
+
+    const outputPath = options.outputPath || compiler.outputPath;
+    const outputFile = path.resolve(outputPath, options.filename);
+
+    fs.outputFileSync(outputFile, options.serialize(manifest));
+  }
+
+  apply(compiler) {
+    const emit = compilation => this.generateManifest(compiler, compilation);
+
+    if (compiler.hooks) {
+      compiler.hooks.emit.tap({ name: this.name, stage: Infinity }, emit);
+    } else {
+      compiler.plugin('emit', emit);
+    }
   }
 }
 
